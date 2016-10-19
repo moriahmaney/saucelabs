18bc165a735079b4e82aa12b85719a614394bc81
Moriah Maney <momaney@cisco.com>
2016-10-19 11:05:09 -0400

    removed file

--- EditDNS.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo 0.0.0.0 www.api.mixpanel.com > %temp%\temphosts.txt
-type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
-copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
