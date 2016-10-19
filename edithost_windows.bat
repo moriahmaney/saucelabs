59b43b557f7033537bea60c3de38a597820e908a
Moriah Maney <momaney@cisco.com>
2016-10-19 11:06:12 -0400

    add file back

---
+++ edithost_windows.bat
@@ -0,0 +1,4 @@
+@echo off
+echo 0.0.0.0 www.api.mixpanel.com > %temp%\temphosts.txt
+type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
+copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
